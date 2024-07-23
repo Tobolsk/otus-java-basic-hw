2fc1a9f747d508f403e239c6e468b4e5b8257282
Robert <robert_tobolsk@mail.ru>
2024-07-23 04:18:35 +0300

    "Причесывание" в Hw01

--- src/main/java/lesson03/Hw01.java
+++ src/main/java/lesson03/Hw01.java
@@ -10,7 +10,7 @@ public class Hw01 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число от 1 до 5");
         byte n = scanner.nextByte();
-        while ((n<1) || (n>5)) {
+        while ((n < 1) || (n > 5)) {
             //System.out.print("\033[H\033[2J");
             System.out.flush();
             System.out.println("Введите число от 1 до 5");
@@ -56,7 +56,7 @@ public class Hw01 {
         }
     }
 
-/*(1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java*/
+    /* (1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java*/
     public static void greetings() {
         System.out.println("Hello");
         System.out.println("World");
@@ -64,21 +64,20 @@ public class Hw01 {
         System.out.println("Java");
     }
 
-/*(2) Реализуйте метод checkSign(..), принимающий в качестве аргументов 3 int переменные a, b и c. Метод должен
-посчитать их сумму, и если она больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном
-случае - “Сумма отрицательная”*/
+    /* (2) Реализуйте метод checkSign(..), принимающий в качестве аргументов 3 int переменные a, b и c. Метод должен
+    посчитать их сумму, и если она больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном
+    случае - “Сумма отрицательная”*/
     public static void checkSign(int a, int b, int c) {
-        if ((a+b+c) >= 0) {
+        if ((a + b + c) >= 0) {
             System.out.println("Сумма положительная");
-        }
-        else {
+        } else {
             System.out.println("Сумма отрицательная");
         }
     }
 
-    // (3) Реализуйте метод selectColor() в теле которого задайте int переменную data с любым начальным
-    // значением. Если data меньше 10 включительно, то в консоль должно быть выведено сообщение “Красный”,
-    // если от 10 до 20 включительно, то “Желтый”, если больше 20 - “Зеленый”;
+    /* (3) Реализуйте метод selectColor() в теле которого задайте int переменную data с любым начальным значением. Если
+    data меньше 10 включительно, то в консоль должно быть выведено сообщение “Красный”, если от 10 до 20 включительно, то
+     “Желтый”, если больше 20 - “Зеленый”;*/
     public static void selectColor(int data) {
         if (data <= 10) {
             System.out.println("Красный");
@@ -89,25 +88,23 @@ public class Hw01 {
         }
     }
 
-    // (4) Реализуйте метод compareNumbers(), в теле которого объявите две int переменные a и b
-    // с любыми начальными значениями. Если a больше или равно b, то необходимо вывести в консоль
-    // сообщение “a >= b”, в противном случае “a < b”;
+    /* (4) Реализуйте метод compareNumbers(), в теле которого объявите две int переменные a и b с любыми начальными
+    значениями. Если a больше или равно b, то необходимо вывести в консоль   сообщение “a >= b”, в противном случае “a <
+     b”;*/
     public static void compareNumbers(int a, int b) {
-        if (a >= b){
+        if (a >= b) {
             System.out.println(a + " >= " + b);
-        }
-        else {
+        } else {
             System.out.println(a + " < " + b);
         }
     }
 
-/*(5) Создайте метод addOrSubtractAndPrint(int initValue, int delta, boolean increment). Если increment = true, то
-метод должен к initValue прибавить delta и отпечатать в консоль     результат, в противном случае - вычесть*/
+    /* (5) Создайте метод addOrSubtractAndPrint(int initValue, int delta, boolean increment). Если increment = true, то
+    метод должен к initValue прибавить delta и отпечатать в консоль     результат, в противном случае - вычесть*/
     public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
         if (increment) {
             System.out.println(initValue + delta);
-        }
-        else {
+        } else {
             System.out.println(initValue - delta);
         }
     }
